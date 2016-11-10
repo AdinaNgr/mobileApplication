878d2dcf4dc38286d0281cbd4b506ca9c879c72a
Adina <negrau.adina@gmail.com>
2016-11-10 04:36:10 -0800

    Make disappear the toolbar on List activity

--- FirstAndroidApp/app/src/main/java/com/example/adina/firstandroidapp/ListActivity.java
+++ FirstAndroidApp/app/src/main/java/com/example/adina/firstandroidapp/ListActivity.java
@@ -26,8 +26,8 @@ public class ListActivity extends AppCompatActivity {
         Log.v("Adapter", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         layoutManager = new LinearLayoutManager(this);
@@ -51,7 +51,7 @@ public class ListActivity extends AppCompatActivity {
             Log.v("main", "onStart");
 
             if (movieList.size() == 0) {
-                movieList.add(new Movie("http://cms.toptenthailand.net/file/picture/20160106233919529/20160106233919529.jpg", "Dark Knight", "2008", "Christopher Nolan", "9.0"));
+                //movieList.add(new Movie("http://cms.toptenthailand.net/file/picture/20160106233919529/20160106233919529.jpg", "Dark ttt", "2008", "Christopher Nolan", "9.0"));
                 movieList.add(new Movie("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Triton_moon_mosaic_Voyager_2_%28large%29.jpg/320px-Triton_moon_mosaic_Voyager_2_%28large%29.jpg", "Harry Potter and the Goblet of Fire", "2005", "Mike Newell", "7.7"));
                 movieList.add(new Movie("http://t0.gstatic.com/images?q=tbn:ANd9GcQhYjUIu2o5v5u3rfJpCq5Cz0Q9WK--XdYxai_N2d0ImohPiIOp", "Titanic", "1997","James Cameron", "7.7"));
                 movieList.add(new Movie("http://static.rogerebert.com/uploads/movie/movie_poster/the-salt-of-the-earth-2015/large_76pQSAYjGMYD6ssHkYCG0zDW9bE.jpg", "The salt of the earth", "2014", "Juliano Ribeiro Salgado", "8.4"));
